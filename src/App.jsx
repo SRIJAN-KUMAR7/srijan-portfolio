82754379601a06625027def88da8e156ead545fd
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-06-10 22:18:12 +0530

    added the loading bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css"
+import { Navbar } from './components/Navbar'
 
 const App = () => {
+  const[isLoaded,setIsLoaded]=useState(false)
   return (
-    <div>
-      
-    </div>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={()=>setIsLoaded(true)}/>}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100` }>
+          {/* <Navbar/>      */}
+        </div>
+    </>
   )
 }
 
