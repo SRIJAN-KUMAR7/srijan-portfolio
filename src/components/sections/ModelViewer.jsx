b56bea2ba6b49b337720abc1b0cddf6a2eeb69eb
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-06-14 20:53:06 +0530

    added the lazyloading for the glb

--- src/components/sections/ModelViewer.jsx
+++ src/components/sections/ModelViewer.jsx
@@ -1,13 +1,29 @@
-import React, { Suspense } from "react";
+import React, { Suspense, lazy } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
-
-const Model = () => {
-  const gltf = useGLTF("/models/space_station_compressed.glb"); 
+import { useEffect, useState } from "react";
+const Model = React.memo(() => {
+  const gltf = useGLTF("/models/space_station_compressed.glb");
   return <primitive object={gltf.scene} scale={0.5} />;
-};
+});
+
+useGLTF.preload("/models/space_station_compressed.glb");
 
 export const ModelViewer = () => {
+  const [shouldRender, setShouldRender] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mobileCheck = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    setIsMobile(mobileCheck);
+    const timeout = setTimeout(() => {
+      setShouldRender(true);
+    }, 100); 
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (!shouldRender || isMobile) return null; 
   return (
     <div className="w-full h-[400px] rounded-xl overflow-hidden shadow-lg">
       <Canvas camera={{ position: [2, 2, 5], fov: 45 }}>
